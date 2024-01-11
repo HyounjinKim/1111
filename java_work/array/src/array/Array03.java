b5d0f182e12eb7faed50cec85f521d829f41cdf6
HyounjinKim <hyoung6658@gmail.com>
2024-01-11 14:36:50 +0900

    240111

--- java_work/array/src/array/Array03.java
+++ java_work/array/src/array/Array03.java
@@ -1,34 +1,28 @@
 package array;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Array03 {
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-
-		int N = sc.nextInt();
-		int[] A = new int[N];
-
-		int min = 0;
-		int max = 0;
-
-		for (int i = 0; i < N; i++) {
-			A[i] += sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(bf.readLine());
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		for(int i = 0;i < N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		for (int i = 0; i < N; i++) {
-			if (i+1 == N) {
-				break;
-			}
-			else if(A[i] > A[i+1]) {
-				max = A[i];
-			}
-			else if(A[i] < A[i+1]) {
-				min = A[i];
-			}
-			
-		}
-		System.out.print(min + " " + max);
-
+		Arrays.sort(arr);
+		
+		System.out.println(arr[0] + " " + arr[N-1]);
 	}
+
+		
 }
